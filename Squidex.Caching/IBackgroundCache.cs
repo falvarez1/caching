0e6bc68c35f9296fa38a8c80b508b6f109ae8420
Sebastian <sebastian@squidex.io>
2021-02-01 17:17:49 +0100

    Background cache.

---
+++ Squidex.Caching/IBackgroundCache.cs
@@ -0,0 +1,17 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System;
+using System.Threading.Tasks;
+
+namespace Squidex.Caching
+{
+    public interface IBackgroundCache
+    {
+        Task<T> GetOrCreateAsync<T>(object key, TimeSpan expiration, Func<object, Task<T>> creator, Func<T, Task<bool>>? isValid = null);
+    }
+}
